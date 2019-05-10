d06fcd274ee6819df05d323920fd7d7714448625
nodaguti <nodaguti@gmail.com>
2019-05-11 00:09:40 +0900

    Fix an issue where rule was not correctly exported

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,9 @@
-export { default, ruleName, messages } from './stylelint-no-unused-selectors';
+import {
+  default as rule,
+  ruleName,
+  messages,
+} from './stylelint-no-unused-selectors';
+
+module.exports = rule;
+module.exports.ruleName = ruleName;
+module.exports.messages = messages;
