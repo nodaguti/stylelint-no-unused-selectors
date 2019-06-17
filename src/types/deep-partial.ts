5ad397f4c6143d5a8b2f2ee9d31e68e6149821ee
nodaguti <nodaguti@gmail.com>
2019-06-17 11:13:13 +0900

    style(types): Remove usages of array generics

--- src/types/deep-partial.ts
+++ src/types/deep-partial.ts
@@ -1,7 +1,7 @@
 export type DeepPartial<T> = {
   [P in keyof T]?: T[P] extends (infer U)[]
     ? DeepPartial<U>[]
-    : T[P] extends ReadonlyArray<infer U>
-    ? ReadonlyArray<DeepPartial<U>>
+    : T[P] extends readonly (infer U)[]
+    ? readonly DeepPartial<U>[]
     : DeepPartial<T[P]>
 };
